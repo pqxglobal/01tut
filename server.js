551b604780df51a656defadf641a3e5d43895d36
pqxglobal <l.r.slegt@gmail.com>
2023-10-30 10:14:55 +0100

    🚧 1:32:36

--- server.js
+++ server.js
@@ -1,36 +1,26 @@
-// How nodejs differs from vanillajs
-// 1) Node runs on a server - not in a browser (backend, not frontend)
-// 2) Thee console is the terminal window
-// 3) Global object instead of a window object
-// 4) Has common core modules
-// 5) CommonJS modules instead of ES6 modules
-// 6) Missing some JS API's like fetch
-console.log('Hello World')
-//console.log(global)
-
-const os = require('os')
+const http = require('http')
 const path = require('path')
-const { add, subtract, multiply, divide, frequency, iavg, cirkelfreq } = require('./math');
-const { log } = require('console');
+const fs = require('fs')
+const fsPromises = require('fs').promises
 
-console.log(add(2,3));
-console.log(subtract(2,3));
-console.log(multiply(2,3));
-console.log(divide(2,3));
-console.log(frequency(50));
-console.log(iavg(4,8,4,-2,-4,-2));
-console.log(cirkelfreq(50) + ' rad / s');
+const logEvents = require('./logEvents')
+const EventEmitter = require('events')
+class Emitter extends EventEmitter {}
+// initialize objects
+const myEmitter = new Emitter()
+
+const PORT = process.env.PORT || 3500
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+})
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 
-// console.log(os.type());
-// console.log(os.version());
-// console.log(os.homedir());
 
-// console.log(__dirname);
-// console.log(__filename);
 
-// console.log(path.dirname(__filename));
-// console.log(path.basename(__filename));
-// console.log(path.extname(__filename));
 
-// console.log(path.parse(__filename));
+// myEmitter.on('log', msg => logEvents(msg))
+
+// myEmitter.emit('log', 'Log event emitted!')
