df4092899347551c3c8349bb0d920b6148570c45
pqxglobal <l.r.slegt@gmail.com>
2023-10-30 01:56:43 +0100

    🚧 1:08:21

--- index.js
+++ index.js
@@ -1,49 +1,8 @@
-const fsPromises = require('fs').promises
-const path = require('path')
+const { format } = require('date-fns')
+const { v4: uuid } = require('uuid')
 
-const fileOps = async () => {
-  try {
-    const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8')
-    console.log(data)
-    await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt'))
-    await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data)
-    await fsPromises.appendFile(
-      path.join(__dirname, 'files', 'promiseWrite.txt'),
-      '\n\nNice to meet you.'
-    )
-    await fsPromises.rename(
-      path.join(__dirname, 'files', 'promiseWrite.txt'),
-      path.join(__dirname, 'files', 'promiseComplete.txt')
-    )
-    const newData = await fsPromises.readFile(
-      path.join(__dirname, 'files', 'promiseComplete.txt'),
-      'utf8'
-    )
-    console.log(newData)
-  } catch (err) {
-    console.error(err)
-  }
-}
+console.log(format(new Date(), 'dd-MM-yyyy\tHH:mm:ss'));
 
-fileOps()
+console.log(uuid());
 
-// fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you.', err => {
-//   if (err) throw err
-//   console.log('Write Complete')
-
-//   fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nYes it is.', err => {
-//     if (err) throw err
-//     console.log('Append Complete')
-
-//     fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newRreply.txt'), err => {
-//       if (err) throw err
-//       console.log('Rename Complete')
-//     })
-//   })
-// })
-
-// exit on uncaught errors
-process.on('uncaughtExeption', err => {
-  console.error(`There was an uncaught error: ${err}`)
-  process.exit(1)
-})
+console.log();
